54bbbe04bc40b0c1b0751076b0350e133abcd332
shion0625 <xkaito0912@gmail.com>
2019-08-21 11:53:15 +0000

    [AC] APG4b APG4b_cn

---
+++ atcoder.jp/APG4b/APG4b_cn/Main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int x, a, b;
+  cin >> x >> a >> b;
+ 
+  // 1.の出力
+  x++;
+  cout << x << endl;
+ // 2.の出力
+  x = x*(a+b);
+  cout << x << endl;
+  // 3.の出力
+  x = x*x;
+  cout << x << endl;
+  // 4.の出力
+  x--;
+  cout << x << endl;
+}
