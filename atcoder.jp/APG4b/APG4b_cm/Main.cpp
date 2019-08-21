9f2691c3058dde1cb39d2d438b90a83052a8d4a2
shion0625 <xkaito0912@gmail.com>
2019-08-21 12:45:43 +0000

    [AC] APG4b APG4b_cm

---
+++ atcoder.jp/APG4b/APG4b_cm/Main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int A, B;
+  cin >> A >> B;
+  //A
+  int i=0;
+  cout << "A:";
+ while(i<A){
+   cout << "]";
+ i++;
+ }
+  cout << endl;
+  int j=0;
+  cout << "B:";
+  while(j<B) {
+  cout << "]";
+    j++;
+  }
+  cout << endl;
+}
