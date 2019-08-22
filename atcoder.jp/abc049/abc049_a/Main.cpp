78f98b1b392beb2f1cf83040ccb560e1f1020066
shion0625 <xkaito0912@gmail.com>
2019-08-22 10:16:11 +0000

    [AC] abc049 abc049_a

---
+++ atcoder.jp/abc049/abc049_a/Main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+char c;
+cin >> c;
+if(c == 'a' || c =='i' || c=='u' || c=='e' || c=='o') {
+ cout << "vowel" << endl;
+} else {
+cout << "consonant" << endl;
+}
+}
