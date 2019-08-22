6a987bf08702ea56e22a6a9be15209b7a61ac527
shion0625 <xkaito0912@gmail.com>
2019-08-22 06:55:59 +0000

    [AC] abc133 abc133_a

---
+++ atcoder.jp/abc133/abc133_a/Main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  int N,A,B;
+  cin >> N >> A >> B;
+  if(N*A >= B) {
+   cout << B << endl;
+  } else {
+    cout << N*A << endl;
+  }
+  return 0;
+}
