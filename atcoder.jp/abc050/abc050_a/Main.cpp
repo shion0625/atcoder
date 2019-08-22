64b9ce4aeec8383aab13f1bafb7f2f8d3abab1f8
shion0625 <xkaito0912@gmail.com>
2019-08-22 10:30:05 +0000

    [AC] abc050 abc050_a

---
+++ atcoder.jp/abc050/abc050_a/Main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+ int a,b;
+char op;
+cin >> a >> op >> b;
+if(op == '+'){
+  cout << a + b << endl;
+} else if(op == '-') {
+cout << a-b << endl;
+}
+}
