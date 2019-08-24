2f62a962917489488120b190c1af33ba9f30a234
shion0625 <xkaito0912@gmail.com>
2019-08-24 16:45:55 +0000

    [AC] abc130 abc130_a

---
+++ atcoder.jp/abc130/abc130_a/Main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  int n,a;
+  cin >> n >> a;
+  if(n >= a) {
+  cout << "10" <<endl;
+  }else{
+    cout << "0" << endl;
+  }
+  return 0;
+}
