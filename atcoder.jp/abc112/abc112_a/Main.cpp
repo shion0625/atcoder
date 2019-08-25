cc447fc5298cce15db56e8ee14c3306fa7c5a869
shion0625 <xkaito0912@gmail.com>
2019-08-25 15:31:48 +0000

    [AC] abc112 abc112_a

---
+++ atcoder.jp/abc112/abc112_a/Main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+int N;
+int a,b;
+cin >> N;
+if(N == 1) {
+  cout << "Hello World" << endl;
+}else if(N == 2) {
+  cin >> a >> b;
+  cout << a+b << endl;
+}
+}
+
