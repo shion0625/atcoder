ad411885383e250a3858eabbbcd328776198c3b3
shion0625 <xkaito0912@gmail.com>
2019-08-25 15:19:26 +0000

    [AC] abc114 abc114_a

---
+++ atcoder.jp/abc114/abc114_a/Main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  int x;
+  cin >> x;
+  if(x == 3){
+  cout << "YES" << endl;
+  }else if(x == 5){
+    cout << "YES" << endl;
+  }else if(x == 7){
+    cout << "YES" <<endl;
+  } else{
+    cout << "NO" << endl;
+  }
+}
