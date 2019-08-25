60f924e8e8bc61807d754c177e2977a005e4099c
shion0625 <xkaito0912@gmail.com>
2019-08-25 15:15:56 +0000

    [AC] abc115 abc115_a

---
+++ atcoder.jp/abc115/abc115_a/Main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  int D;
+  cin >> D;
+  if(D==25){
+    cout << "Christmas" << endl;
+  } else if(D==24){
+    cout << "Christmas Eve" << endl;
+  }else if(D==23){
+    cout << "Christmas Eve Eve" << endl;
+  }else if(D==22){
+    cout << "Christmas Eve Eve Eve" << endl;
+  }
+}
