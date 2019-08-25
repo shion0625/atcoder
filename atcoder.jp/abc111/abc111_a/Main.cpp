a0d6aab320408f8057a7f0d1791b51a45d52615b
shion0625 <xkaito0912@gmail.com>
2019-08-25 16:24:56 +0000

    [AC] abc111 abc111_a

---
+++ atcoder.jp/abc111/abc111_a/Main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(void) {
+  string n;
+  cin >> n;
+  for(int i=0; i<3; i++) {
+    if (n[i] == '1') {
+      n[i] = '9';
+    } else if(n[i] == '9'){
+      n[i] = '1';
+    }
+  }
+  cout << n << endl;
+}
