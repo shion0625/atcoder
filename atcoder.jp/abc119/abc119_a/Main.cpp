501facf08dd91bbbf81b86888cff00dec0220d84
shion0625 <xkaito0912@gmail.com>
2019-08-25 13:18:39 +0000

    [AC] abc119 abc119_a

---
+++ atcoder.jp/abc119/abc119_a/Main.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  int y,m,d;
+  char c1,c2;
+  cin >> y >> c1 >> m >> c2 >> d;
+
+  cout << (m <= 4  ? "Heisei":"TBD") << endl;
+}
