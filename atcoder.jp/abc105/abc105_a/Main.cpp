29dab223d35ac9125414c34b20e380556eed31ca
shion0625 <xkaito0912@gmail.com>
2019-09-10 07:40:48 +0000

    [AC] abc105 abc105_a

---
+++ atcoder.jp/abc105/abc105_a/Main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(void){
+  int n,k;
+  cin >> n >> k;
+  if(n % k == 0){
+    cout << 0 << endl;
+  } else {
+    cout << 1 << endl;
+  }
+  return 0;
+}
