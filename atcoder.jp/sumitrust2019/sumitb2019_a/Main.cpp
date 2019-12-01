53a32d07f4eea3da5993b1cab04d202290a62feb
shion0625 <xkaito0912@gmail.com>
2019-12-01 12:12:26 +0000

    [AC] sumitrust2019 sumitb2019_a

---
+++ atcoder.jp/sumitrust2019/sumitb2019_a/Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <string>
+#include <map>
+#include <sstream>
+#include <cmath>
+#include <iomanip>
+#include <stdexcept>
+#include <utility>
+// #include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define rep(i,h) for(int i = 0; i < h; i++)
+#define rep1(i,h) for(int i = 0; i <= h; i++)
+#define rep2(i,n,h) for(int i = n; i < h; i++)
+#define rep3(l,n,h) for(int l = n; i <= h; i++)
+using namespace std;
+
+
+
+int main() {
+    int m ,d,n, h;
+    cin >> m >> d >> n >>h;
+    if(m != n) {
+        cout << 1 << endl;
+    } else {
+        cout << 0 << endl;
+    }
+
+    return 0;
+}
