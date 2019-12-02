81d550872a7c7f5782a153b3b100e4fd74448bf4
shion0625 <xkaito0912@gmail.com>
2019-12-02 02:27:24 +0000

    [AC] abc111 abc111_b

---
+++ atcoder.jp/abc111/abc111_b/Main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <string>
+#include <map>
+#include <sstream>
+#include <cmath>
+#include <cstdio>
+#include <iomanip>
+#include <stdexcept>
+#include <utility>
+// #include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define rep(i,h) for(int i = 0; i < h; ++i)
+#define rep1(i,h) for(int i = 0; i <= h; ++i)
+#define rep2(i,n,h) for(int i = n; i < h; ++i)
+#define rep3(l,n,h) for(int l = n; i <= h; ++i)
+using namespace std;
+
+
+    int main() {
+        double n;
+        cin >> n;
+        int k = ceil(n / 111.0);
+        cout << k * 111 ;
+        return 0;
+    }
