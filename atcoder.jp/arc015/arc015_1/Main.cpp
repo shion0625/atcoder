c052e570ada807b73a6b8c95f9518ecf69ad83fe
shion0625 <xkaito0912@gmail.com>
2019-12-04 15:41:08 +0000

    [AC] arc015 arc015_1

---
+++ atcoder.jp/arc015/arc015_1/Main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define rep(i,h) for(int i = 0; i < h; ++i)
+#define rep1(i,h) for(int i = 0; i <= h; ++i)
+#define rep2(i,n,h) for(int i = n; i < h; ++i)
+#define rep3(l,n,h) for(int l = n; l <= h; ++l)
+typedef pair<int,int> int_pair;
+
+
+int main() {
+    double n;
+    cin >> n;
+    double ans = 9*n/5 + 32;
+    cout << ans<< endl;
+     return 0;
+    }
