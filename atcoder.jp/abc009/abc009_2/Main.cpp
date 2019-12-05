215e33d969e05966e342061fee1d40a3e21a9711
shion0625 <xkaito0912@gmail.com>
2019-12-05 05:35:56 +0000

    [AC] abc009 abc009_2

---
+++ atcoder.jp/abc009/abc009_2/Main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define rep(i,h) for(int i = 0; i < h; ++i)
+#define rep1(i,h) for(int i = 0; i <= h; ++i)
+#define rep2(i,n,h) for(int i = n; i < h; ++i)
+#define rep3(l,n,h) for(int l = n; l <= h; ++l)
+typedef pair<int,int> int_pair;
+typedef pair<string,string> string_pair;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int>  a(n);
+    rep(i,n) cin >> a.at(i);
+    sort(a.rbegin(),a.rend());
+    rep(i,n) {
+        if(a.at(i) != a.at(i+1)) {
+            cout << a.at(i+1) << endl;
+            return 0;
+        }
+    }
+    return 0;
+}
+
