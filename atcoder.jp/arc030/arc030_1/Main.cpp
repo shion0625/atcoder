ea86c44017219083d7c4722ef2b99bcfbe841451
shion0625 <xkaito0912@gmail.com>
2019-12-09 04:13:25 +0000

    [AC] arc030 arc030_1

---
+++ atcoder.jp/arc030/arc030_1/Main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define rep(i,h) for(int i = 0; i < h; ++i)
+#define rep1(i,h) for(int i = 0; i <= h; ++i)
+#define rep2(i,n,h) for(int i = n; i < h; ++i)
+#define rep3(l,n,h) for(int l = n; l <= h; ++l)
+typedef pair<int,int> int_pair;
+typedef pair<string,string> string_pair;
+
+
+int main(void) {
+     int n,k;
+     cin >> n >> k;
+     if(n/2 >= k) cout << "YES" << endl;
+     else cout << "NO" << endl;
+    return 0;
+}
+
