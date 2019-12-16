a59f5ee38041364ea1083b76a0151dd0e66b5b81
shion0625 <xkaito0912@gmail.com>
2019-12-16 05:18:13 +0000

    [AC] abc086 abc086_b

---
+++ atcoder.jp/abc086/abc086_b/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define rep(i,h) for(int i = 0; i < h; ++i)
+#define rep1(i,h) for(int i = 0; i <= h; ++i)
+#define rep2(i,k,h) for(int i = k; i < h; ++i)
+#define rep3(l,k,h) for(int l = k; l <= h; ++l)
+#define INF LLong_MAX // long long INF
+#define inf INT_MAX // int inf
+
+typedef pair<int,int> int_pair;
+typedef pair<string,string> string_pair;
+
+
+int main() {
+    string a,b;
+    cin >> a >> b;
+    a += b;
+    int num = stoi(a);
+    rep3(i,1,num) {
+        if(num == i*i) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
+}
