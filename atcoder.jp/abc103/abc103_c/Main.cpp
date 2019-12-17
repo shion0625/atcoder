265271b792a78577ac2a0d3283b2427d1ed56529
shion0625 <xkaito0912@gmail.com>
2019-12-17 01:56:33 +0000

    [AC] abc103 abc103_c

---
+++ atcoder.jp/abc103/abc103_c/Main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define rep(i,h) for(int i = 0; i < h; ++i)
+#define rep1(i,h) for(int i = 0; i <= h; ++i)
+#define rep2(i,k,h) for(int i = k; i < h; ++i)
+#define rep3(l,k,h) for(int l = k; l <= h; ++l)
+#define INF LLong_MAX // long long INF
+#define inf INT_MAX // int inf
+
+typedef pair<int,int> int_pair;
+typedef pair<string,string> string_pair;
+
+
+int main() {
+    int n;
+    cin >> n;
+    int ans = -n,a;
+    rep(i,n) {
+        cin >> a;
+        ans += a;
+    }
+    cout << ans << endl;
+}
