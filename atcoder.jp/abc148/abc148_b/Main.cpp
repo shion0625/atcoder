6d03890f31094eb3fe0b48852e2878affdcf4f65
shion0625 <xkaito0912@gmail.com>
2019-12-22 12:05:01 +0000

    [AC] abc148 abc148_b

---
+++ atcoder.jp/abc148/abc148_b/Main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define rep(i,h) for(int i = 0; i < h; ++i)
+#define rep1(i,h) for(int i = 0; i <= h; ++i)
+#define rep2(i,k,h) for(int i = k; i < h; ++i)
+#define rep3(i,k,h) for(int i = k; i <= h; ++i)
+#define INF LLong_MAX // long long INF
+#define inf INT_MAX // int inf
+
+typedef pair<int,int> int_pair;
+typedef pair<string,string> string_pair;
+
+
+
+int main() {
+    int n;
+    cin >> n;
+    string s,t;
+    cin >> s >> t;
+    string ans;
+    for(int i = 0; i < n; i++) {
+        ans += s[i];
+        ans += t[i];
+    }
+    cout << ans << endl;
+    return 0;
+}
