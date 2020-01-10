75e40fccb65adf5f137186bae195fc357d912fac
shion0625 <xkaito0912@gmail.com>
2020-01-10 12:07:33 +0000

    [AC] abc150 abc150_a

---
+++ atcoder.jp/abc150/abc150_a/Main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define rep(i,h) for(int i = 0; i < h; ++i)
+#define rep1(i,h) for(int i = 0; i <= h; ++i)
+#define rep2(i,k,h) for(int i = k; i < h; ++i)
+#define rep3(i,k,h) for(int i = k; i <= h; ++i)
+#define INF LLong_MAX // long long INF
+#define inf INT_MAX // int inf
+
+typedef pair<int,int> int_pair;
+typedef pair<string,string> string_pair;
+
+
+
+int main() {
+    int k,x;
+    cin >> k >> x;
+    if(500 * k >= x) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+    return 0;
+}
