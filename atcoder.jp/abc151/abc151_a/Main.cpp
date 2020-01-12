347ebdbb02495e42ff9509349ef79c2b292fce75
shion0625 <xkaito0912@gmail.com>
2020-01-12 12:11:17 +0000

    [AC] abc151 abc151_a

---
+++ atcoder.jp/abc151/abc151_a/Main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define ld long double
+#define rep(i,h) for(int i = 0; i < h; ++i)
+#define rep1(i,h) for(int i = 0; i <= h; ++i)
+#define rep2(i,k,h) for(int i = k; i < h; ++i)
+#define rep3(i,k,h) for(int i = k; i <= h; ++i)
+#define INF LLong_MAX // long long INF
+#define inf INT_MAX // int inf
+
+typedef pair<int,int> int_pair;
+typedef pair<string,string> string_pair;
+
+
+int main() {
+    char a;
+    cin >> a;
+    int b;
+    b = a+1;
+    char c;
+    c = b;
+    cout << c << endl;
+    return 0;
+}
