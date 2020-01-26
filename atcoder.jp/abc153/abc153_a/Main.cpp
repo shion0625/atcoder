40db8e3829fd687649abcacd8ef84299cdfd91c2
shion0625 <xkaito0912@gmail.com>
2020-01-26 12:03:32 +0000

    [AC] abc153 abc153_a

---
+++ atcoder.jp/abc153/abc153_a/Main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define ld long double
+#define rep(i,h) for(int i = 0; i < h; ++i)
+#define rep1(i,h) for(int i = 0; i <= h; ++i)
+#define rep2(i,k,h) for(int i = k; i < h; ++i)
+#define rep3(i,k,h) for(int i = k; i <= h; ++i)
+#define INF LLong_MAX // long long INF
+#define inf INT_MAX // int inf
+
+typedef pair<int,int> int_pair;
+typedef pair<string,string> string_pair;
+
+
+int main() {
+    int h,a;
+    cin >> h >> a;
+    int count = 0;
+    while(h>0) {
+        h-=a;
+        count++;
+    }
+    cout << count<< endl;
+}
