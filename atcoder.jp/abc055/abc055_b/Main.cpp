ad2f6178ed5122343025e222f4e396a5e6096626
shion0625 <xkaito0912@gmail.com>
2020-01-26 04:34:01 +0000

    [AC] abc055 abc055_b

---
+++ atcoder.jp/abc055/abc055_b/Main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define ld long double
+#define rep(i,h) for(int i = 0; i < h; ++i)
+#define rep1(i,h) for(int i = 0; i <= h; ++i)
+#define rep2(i,k,h) for(int i = k; i < h; ++i)
+#define rep3(i,k,h) for(int i = k; i <= h; ++i)
+#define INF LLong_MAX // long long INF
+#define inf INT_MAX // int inf
+
+typedef pair<int,int> int_pair;
+typedef pair<string,string> string_pair;
+
+
+int main() {
+    int n;
+    cin >> n;
+    ll ans = 1, mod = 1e9+7;
+    for(int i = 1; i < n+1; i++) {
+         ans *= i;
+         ans %= mod;
+    }
+    cout << ans << endl;
+}
