03407e5ca36433fae91148a9286143291815b168
shion0625 <xkaito0912@gmail.com>
2020-02-10 11:40:25 +0000

    [AC] abc041 abc041_b

---
+++ atcoder.jp/abc041/abc041_b/Main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define ld long double
+#define rep(i,h) for(int i = 0; i < h; ++i)
+#define rep1(i,h) for(int i = 0; i <= h; ++i)
+#define rep2(i,k,h) for(int i = k; i < h; ++i)
+#define rep3(i,k,h) for(int i = k; i <= h; ++i)
+#define INF LONG_LONG_MAX // long long INF
+#define inf INT_MAX // int inf
+
+typedef pair<int,int> int_pair;
+typedef pair<string,string> string_pair;
+
+int main(){
+    ll a, b, c;
+    ll Inf = 1e9+7;
+    cin >> a >> b >> c;
+    cout << a * b % Inf * c % Inf << endl;
+}
