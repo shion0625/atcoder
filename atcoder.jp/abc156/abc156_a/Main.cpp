025bdfce0a5ab563458c7967da204fd2daa52846
shion0625 <xkaito0912@gmail.com>
2020-02-22 12:03:22 +0000

    [AC] abc156 abc156_a

---
+++ atcoder.jp/abc156/abc156_a/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define ld long double
+#define rep(i,h) for(int i = 0; i < h; ++i)
+#define rep1(i,h) for(int i = 0; i <= h; ++i)
+#define rep2(i,k,h) for(int i = k; i < h; ++i)
+#define rep3(i,k,h) for(int i = k; i <= h; ++i)
+#define INF LONG_LONG_MAX // long long INF
+#define inf INT_MAX // int inf
+
+typedef pair<int,int> int_pair;
+typedef pair<string,string> string_pair;
+
+
+int main() {
+    int n, r;
+    cin >> n >> r;
+    if(n >= 10) {
+        cout << r << endl;
+        return 0;
+    } else {
+        cout << r + (100 *(10 - n)) << endl;
+    }
+    return 0;
+}
+
