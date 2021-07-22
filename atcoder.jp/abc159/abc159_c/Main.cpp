8fc75b52450c03fff597a412d39c0223a615f373
shion0625 <xkaito0912@gmail.com>
2021-07-22 02:22:34 +0000

    [AC] abc159 abc159_c

---
+++ atcoder.jp/abc159/abc159_c/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "iostream"
+#include <vector>
+using namespace std;
+
+int main()
+{
+    double l;
+    cin >> l;
+    double k = l/3;
+    double ans = pow(k , 3);
+    cout << fixed << setprecision(15) << ans << endl;
+
+    return 0;
+}
+
