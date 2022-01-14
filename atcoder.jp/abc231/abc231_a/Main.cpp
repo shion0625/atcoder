a3951ff76b10278c6c55098991dd39e0f34048ea
shion0625 <xkaito0912@gmail.com>
2022-01-14 11:54:16 +0000

    [AC] abc231 abc231_a

---
+++ atcoder.jp/abc231/abc231_a/Main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define rep(i,n) for(int i = 0; i < (n); ++i)
+#define INF LONG_LONG_MAX // ll INF
+#define inf INT_MAX // int inf
+
+typedef pair<int,int> int_pair;
+typedef pair<string,string> string_pair;
+
+
+int main() {
+    double d;
+    cin >> d;
+    double ans = d/100;
+    cout << ans;
+    return 0;
+}
+
