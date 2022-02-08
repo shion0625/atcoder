9a69ec34a4093152fd922eaad03a64667a9abc5c
shion0625 <xkaito0912@gmail.com>
2022-02-08 11:27:58 +0000

    [AC] typical90 typical90_ag

---
+++ atcoder.jp/typical90/typical90_ag/Main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(int i = 0; i < (n); ++i)
+#define rep1(i,a,b) for(int i=a; i<b; ++i)
+# define ALL(x) (x).begin(), (x).end()
+# define SZ(x) ((int)(x).size)
+
+//エイリアス
+typedef long unsigned long ll;
+typedef long unsigned long ull;
+typedef long double ld;
+typedef pair<ll, ll> ll_pair;
+typedef pair<double, double> DoP;
+typedef pair<int, int> int_pair;
+typedef pair<int, string> pair;
+
+const ll INF = LONG_LONG_MAX; // ll INF
+const int inf = INT_MAX; // int inf
+
+//typedef pair<string,string> string_pair;
+
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+//void printVector(const vector<int>& vec) {
+//    cout << endl;
+//    for (int value : vec) {
+//        cout << value << " ";
+//    }
+//    cout << endl;
+//}
+
+//vector<vector<int>> vec(n, vector<int>(n));
+//set<string> st;
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(false);
+    int h, w;
+    cin >> h >> w;
+    //範囲内に完全に含まれる2*2の範囲が無いので全てを点灯させる事ができる。
+    if(h==1 || w ==1){
+        cout << h*w << endl;
+    }else {
+        cout << ((h+1)/2)*((w+1)/2) << endl;
+    }
+    return 0;
+}
