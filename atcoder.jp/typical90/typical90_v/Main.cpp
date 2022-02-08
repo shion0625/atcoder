457be5371b4901c5a90ebb2ac7263a5439e8afe4
shion0625 <xkaito0912@gmail.com>
2022-02-08 08:35:29 +0000

    [AC] typical90 typical90_v

---
+++ atcoder.jp/typical90/typical90_v/Main.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+void _main();
+int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+
+#define rep(i,n) for(int i = 0; i < (n); ++i)
+#define rep1(i,a,b) for(int i=a; i<b; ++i)
+# define ALL(x) (x).begin(), (x).end()
+# define SZ(x) ((int)(x).size)
+
+//エイリアス
+typedef long unsigned long ll;
+typedef long unsigned long ull;
+typedef long double ld;
+typedef pair<ll, ll> ll_pair;
+typedef pair<double, double> DoP;
+typedef pair<int, int> int_pair;
+typedef pair<int, string> pair;
+
+const ll INF = LONG_LONG_MAX; // ll INF
+const int inf = INT_MAX; // int inf
+
+//typedef pair<string,string> string_pair;
+
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+//void printVector(const vector<int>& vec) {
+//    cout << endl;
+//    for (int value : vec) {
+//        cout << value << " ";
+//    }
+//    cout << endl;
+//}
+
+//vector<vector<int>> vec(n, vector<int>(n));
+
+void _main() {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    ll g = gcd(a, gcd(b,c));
+    cout << (a/g-1)+(b/g-1)+(c/g-1);
+
+}
