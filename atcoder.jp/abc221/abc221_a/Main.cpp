6e2d60c0554ea1d2dcc9012097b6e0d14b8abffd
shion0625 <xkaito0912@gmail.com>
2022-02-19 14:28:46 +0000

    [AC] abc221 abc221_a

---
+++ atcoder.jp/abc221/abc221_a/Main.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(int i = 0; i < (n); ++i)
+#define rep1(i,a,b) for(ll i=a; i<b; ++i)
+#define repl(i,n) for(ll i = 0; i < (n); ++i)
+#define rep1l(i,a,b) for(ll i=a; i<b; ++i)
+
+# define ALL(x) (x).begin(), (x).end()
+# define SZ(x) ((int)(x).size)
+
+//エイリアス
+typedef long long ll;
+typedef long unsigned long ull;
+typedef long double ld;
+typedef pair<ll, ll> ll_pair;
+typedef pair<double, double> DoP;
+typedef pair<int, int> int_pair;
+typedef pair<int, string> pair;
+
+const ll INF = LONG_LONG_MAX; // ll INF
+const int inf = INT_MAX; // int inf
+
+//typedef pair<string,string> string_pair;
+
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+//void printVector(const vector<int>& vec) {
+//    cout << endl;
+//    for (int value : vec) {
+//        cout << value << " ";
+//    }
+//    cout << endl;
+//}
+
+//vector<vector<int>> vec(n, vector<int>(n));
+//set<string> st;
+//  map<key, value> mp;
+//   mp[key] = value;
+
+ll getDistance(ll x, ll y, ll x2, ll y2) {
+    ll distance = (x2 - x) * (x2 - x) + (y2 - y) * (y2 - y);
+    return (ll) distance;
+}
+int main() {
+    cin.tie(0); ios::sync_with_stdio(false);
+    int a,b;
+    cin >> a >> b;
+    int k = a-b;
+    int ans=1;
+    rep(i,k){
+        ans*=32;
+    }
+    cout << ans;
+
+    return 0;
+}
