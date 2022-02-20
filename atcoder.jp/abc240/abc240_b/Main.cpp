2e03164669b22063090cc8df8964bafe9adeb49f
shion0625 <xkaito0912@gmail.com>
2022-02-20 12:04:11 +0000

    [AC] abc240 abc240_b

---
+++ atcoder.jp/abc240/abc240_b/Main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(int i = 0; i < (n); ++i)
+#define rep1(i,a,b) for(int i=a; i<b; ++i)
+#define repl(i,n) for(ll i = 0; i < (n); ++i)
+#define rep1l(i,a,b) for(ll i=a; i<b; ++i)
+
+# define ALL(x) (x).begin(), (x).end()
+# define SZ(x) ((int)(x).size)
+
+//エイリアス
+typedef long long ll;
+typedef long unsigned long ull;
+typedef long double ld;
+typedef pair<ll, ll> ll_pair;
+typedef pair<double, double> DoP;
+typedef pair<int, int> int_pair;
+typedef pair<int, string> pair;
+
+const ll INF = LONG_LONG_MAX; // ll INF
+const int inf = INT_MAX; // int inf
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+
+
+int main() {
+    cin.tie(0); ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    set<int> a;
+    rep(i,n){
+        int k;
+        cin >> k;
+        a.insert(k);
+    }
+    cout << a.size();
+
+    return 0;
+}
