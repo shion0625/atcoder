de10d26f41938534da1b1d68df7ab8a4478d6506
shion0625 <xkaito0912@gmail.com>
2022-02-20 14:11:56 +0000

    [AC] typical90 typical90_n

---
+++ atcoder.jp/typical90/typical90_n/Main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(int i = 0; i < (n); ++i)
+#define rep1(i,a,b) for(int i=a; i<b; ++i)
+#define repl(i,n) for(ll i = 0; i < (n); ++i)
+#define rep1l(i,a,b) for(ll i=a; i<b; ++i)
+
+# define ALL(x) (x).begin(), (x).end()
+# define SZ(x) ((int)(x).size)
+
+//エイリアス
+typedef long long ll;
+typedef long unsigned long ull;
+typedef long double ld;
+typedef pair<ll, ll> ll_pair;
+typedef pair<double, double> DoP;
+typedef pair<int, int> int_pair;
+typedef pair<int, string> pair;
+
+const ll INF = LONG_LONG_MAX; // ll INF
+const int inf = INT_MAX; // int inf
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b(n);
+    rep(i,n){
+        cin >> a[i];
+    }
+    rep(i,n){
+        cin >> b[i];
+    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    ll ans = 0;
+    rep(i,n){
+        ans+=abs(a[i]-b[i]);
+    }
+    cout << ans << endl;
+    return 0;
+}
