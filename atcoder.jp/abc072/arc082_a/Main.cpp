54695ca16703712627dd57704d4e3970319593a3
shion0625 <xkaito0912@gmail.com>
2022-02-25 05:38:38 +0000

    [AC] abc072 arc082_a

---
+++ atcoder.jp/abc072/arc082_a/Main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(int i = 0; i < (n); ++i)
+#define rep1(i,a,b) for(int i=a; i<b; ++i)
+#define repl(i,n) for(ll i = 0; i < (n); ++i)
+#define rep1l(i,a,b) for(ll i=a; i<b; ++i)
+
+# define ALL(x) (x).begin(), (x).end()
+# define SZ(x) ((int)(x).size)
+
+//エイリアス
+typedef long long ll;
+typedef long unsigned long ull;
+typedef long double ld;
+typedef pair<ll, ll> ll_pair;
+typedef pair<double, double> DoP;
+typedef pair<int, int> int_pair;
+typedef pair<int, string> pair;
+
+const ll INF = LONG_LONG_MAX; // ll INF
+const int inf = INT_MAX; // int inf
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(1e5+5, 0);
+    rep(i,n){
+        int ai;
+        cin >> ai;
+        a[ai]++;
+        a[ai+1]++;
+        a[ai+2]++;
+    }
+
+    sort(a.rbegin(),a.rend());
+    cout << a[0] << endl;
+    return 0;
+}
