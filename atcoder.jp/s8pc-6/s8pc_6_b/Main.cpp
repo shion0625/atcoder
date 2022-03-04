52908bde8967cab7ed1b25619b3a9520b408f47e
shion0625 <xkaito0912@gmail.com>
2022-03-04 10:04:25 +0000

    [AC] s8pc-6 s8pc_6_b

---
+++ atcoder.jp/s8pc-6/s8pc_6_b/Main.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(int i = 0; i < (n); ++i)
+#define rep1(i,a,b) for(int i=a; i<b; ++i)
+#define repl(i,n) for(ll i = 0; i < (n); ++i)
+#define rep1l(i,a,b) for(ll i=a; i<b; ++i)
+
+# define ALL(x) (x).begin(), (x).end()
+# define SZ(x) ((int)(x).size)
+
+//エイリアス
+typedef long long ll;
+typedef long unsigned long ull;
+typedef long double ld;
+typedef pair<ll, ll> ll_pair;
+typedef pair<double, double> DoP;
+typedef pair<int, int> int_pair;
+typedef pair<int, string> pair;
+
+const ll INF = LONG_LONG_MAX; // ll INF
+const int inf = INT_MAX; // int inf
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+    int n;cin >> n;
+    vector<int> a(n),b(n);
+    ll asb = 0;
+    rep(i,n) {
+        cin >> a[i] >> b[i];
+        asb += (b[i]-a[i]);
+    }
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+
+    rep(i,n){
+        asb+=(abs(a[n/2] - a[i]));
+        asb+=(abs(b[n/2] - b[i]));
+    }
+    cout << asb << endl;
+
+    return 0;
+}
