87e4e6d1460846069fc829c966ebf1556cc05da6
shion0625 <xkaito0912@gmail.com>
2022-03-05 15:46:18 +0000

    [AC] abc126 abc126_c

---
+++ atcoder.jp/abc126/abc126_c/Main.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(int i = 0; i < (n); ++i)
+#define rep1(i,a,b) for(int i=a; i<b; ++i)
+#define repl(i,n) for(ll i = 0; i < (n); ++i)
+#define rep1l(i,a,b) for(ll i=a; i<b; ++i)
+
+# define ALL(x) (x).begin(), (x).end()
+# define SZ(x) ((int)(x).size)
+
+//エイリアス
+typedef long long ll;
+typedef long unsigned long ull;
+typedef long double ld;
+typedef pair<ll, ll> ll_pair;
+typedef pair<double, double> DoP;
+typedef pair<int, int> int_pair;
+typedef pair<int, string> pair;
+
+const ll INF = LONG_LONG_MAX; // ll INF
+const int inf = INT_MAX; // int inf
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+    ld n,k;
+    cin >> n >> k;
+    ld ans =0;
+    rep1(i,1,n+1){
+        int y = i;
+        ld q =1.0/n;
+        while(y<k){
+            y*=2;
+            q/=2;
+        }
+        ans +=q;
+    }
+    cout << fixed << setprecision(12) <<ans << endl;
+
+    return 0;
+}
