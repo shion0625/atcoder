8396dc53ab2cd9896801f2d8f63bab201f2f68ab
shion0625 <xkaito0912@gmail.com>
2022-03-06 05:57:36 +0000

    [AC] abc127 abc127_c

---
+++ atcoder.jp/abc127/abc127_c/Main.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(int i = 0; i < (n); ++i)
+#define rep1(i,a,b) for(int i=a; i<b; ++i)
+#define repl(i,n) for(ll i = 0; i < (n); ++i)
+#define rep1l(i,a,b) for(ll i=a; i<b; ++i)
+
+# define ALL(x) (x).begin(), (x).end()
+# define SZ(x) ((int)(x).size)
+
+//エイリアス
+typedef long long ll;
+typedef long unsigned long ull;
+typedef long double ld;
+typedef pair<ll, ll> ll_pair;
+typedef pair<double, double> DoP;
+typedef pair<int, int> int_pair;
+typedef pair<int, string> pair;
+
+const ll INF = LONG_LONG_MAX; // ll INF
+const int inf = INT_MAX; // int inf
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+    int n,m;
+    cin >> n >> m;
+    int lmax=0,rmin=1e6;
+    rep(i,m){
+        int l,r;
+        cin >> l >> r;
+        lmax=max(lmax,l);
+        rmin=min(rmin,r);
+    }
+    if(rmin >= lmax){
+        cout << rmin - lmax +1 << endl;
+    }else {
+        cout << 0 << endl;
+    }
+
+    return 0;
+}
