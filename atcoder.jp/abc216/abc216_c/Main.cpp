99783ea08c730c97b512592a785edcd671786728
shion0625 <xkaito0912@gmail.com>
2022-03-06 05:32:38 +0000

    [AC] abc216 abc216_c

---
+++ atcoder.jp/abc216/abc216_c/Main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(int i = 0; i < (n); ++i)
+#define rep1(i,a,b) for(int i=a; i<b; ++i)
+#define repl(i,n) for(ll i = 0; i < (n); ++i)
+#define rep1l(i,a,b) for(ll i=a; i<b; ++i)
+
+# define ALL(x) (x).begin(), (x).end()
+# define SZ(x) ((int)(x).size)
+
+//エイリアス
+typedef long long ll;
+typedef long unsigned long ull;
+typedef long double ld;
+typedef pair<ll, ll> ll_pair;
+typedef pair<double, double> DoP;
+typedef pair<int, int> int_pair;
+typedef pair<int, string> pair;
+
+const ll INF = LONG_LONG_MAX; // ll INF
+const int inf = INT_MAX; // int inf
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+    ll n; cin >>n;
+    string ans;
+    while(n){
+        if(n&1){
+            ans+="A";
+            n--;
+        }else{
+            ans+="B";
+            n >>=1;//シフト演算子で書くとこうなる    これと同じ意味n/=2;
+        }
+    }
+    reverse(ans.begin(), ans.end());
+    cout << ans << endl;
+    return 0;
+}
