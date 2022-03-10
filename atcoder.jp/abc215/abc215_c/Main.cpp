14f6fdffdf141cd43e4f2702a719e37cbae82ab8
shion0625 <xkaito0912@gmail.com>
2022-03-10 15:10:51 +0000

    [AC] abc215 abc215_c

---
+++ atcoder.jp/abc215/abc215_c/Main.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,b) for(int i=0; i<b; ++i)
+#define rep1(i,a,b) for(int i=a; i<b; ++i)
+#define rrep(i,a,b) for(int i=a; i>b; --i)
+
+#define repl(i,b) for(ll i=0; i<b; ++i)
+#define repl1(i,a,b) for(ll i=a; i<b; ++i)
+#define rrepl1(i,a,b) for(ll i=a; i>b; --i)
+
+# define ALL(x) (x).begin(), (x).end()
+# define SZ(x) ((int)(x).size)
+
+//エイリアス
+typedef long long ll;
+typedef long unsigned long ull;
+typedef long double ld;
+typedef pair<ll, ll> ll_pair;
+typedef pair<double, double> DoP;
+typedef pair<int, int> int_pair;
+typedef pair<int, string> pair;
+
+const ll INF = LONG_LONG_MAX; // ll INF
+const int inf = INT_MAX; // int inf
+const int MOD = 1000000007;
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+    string s;
+    int t;
+    cin >> s >> t;
+    sort(s.begin(), s.end());
+    int i=0;
+    do {
+        i++;
+        for(auto x : s){
+            if(i==t) cout << x;
+        }
+    } while( next_permutation(s.begin(), s.end()) );     // 次の順列を生成
+    return 0;
+}
