9ba6665c1a4c0c5b0e36ea9297c867cb57f9f772
shion0625 <xkaito0912@gmail.com>
2022-03-12 12:11:36 +0000

    [AC] abc243 abc243_a

---
+++ atcoder.jp/abc243/abc243_a/Main.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,b) for(int i=0; i<b; ++i)
+#define rep1(i,a,b) for(int i=a; i<b; ++i)
+#define rrep(i,a,b) for(int i=a; i>b; --i)
+
+#define repl(i,b) for(ll i=0; i<b; ++i)
+#define repl1(i,a,b) for(ll i=a; i<b; ++i)
+#define rrepl1(i,a,b) for(ll i=a; i>b; --i)
+
+# define ALL(x) (x).begin(), (x).end()
+# define SZ(x) ((int)(x).size)
+
+//エイリアス
+typedef long long ll;
+typedef long unsigned long ull;
+typedef long double ld;
+typedef pair<ll, ll> ll_pair;
+typedef pair<double, double> DoP;
+typedef pair<int, int> int_pair;
+typedef pair<int, string> pair;
+
+const ll INF = LONG_LONG_MAX; // ll INF
+const int inf = INT_MAX; // int inf
+const int MOD = 1000000007;
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+    int v,a,b,c;
+    cin >>  v >> a >> b >> c;
+    int i=0;
+    while(v>=0){
+        if(i%3==0){
+            v-=a;
+        }
+        else if(i%3==1) {
+            v-=b;
+        }
+        else if(i%3==2) {
+            v-=c;
+        }
+        if(v < 0){
+            if(i%3==0) {
+                cout << "F";
+                return 0;
+            }
+            if(i%3==1) {
+                cout << "M";
+                return 0;
+
+            }
+            if(i%3==2) {
+                cout << "T";
+                return 0;
+
+            }
+        }
+        i++;
+    }
+    return 0;
+}
