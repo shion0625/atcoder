af9dc57b083eb56633c61e60b59183d607fba7a1
shion0625 <xkaito0912@gmail.com>
2022-03-13 11:43:40 +0000

    [AC] abc227 abc227_c

---
+++ atcoder.jp/abc227/abc227_c/Main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,b) for(int i=0; i<b; ++i)
+#define rep1(i,a,b) for(int i=a; i<b; ++i)
+#define rrep(i,a,b) for(int i=a; i>b; --i)
+
+#define repl(i,b) for(ll i=0; i<b; ++i)
+#define repl1(i,a,b) for(ll i=a; i<b; ++i)
+#define rrepl1(i,a,b) for(ll i=a; i>b; --i)
+
+# define ALL(x) (x).begin(), (x).end()
+# define SZ(x) ((int)(x).size)
+
+//エイリアス
+typedef long long ll;
+typedef long unsigned long ull;
+typedef long double ld;
+typedef pair<ll, ll> ll_pair;
+typedef pair<double, double> DoP;
+typedef pair<int, int> int_pair;
+typedef pair<int, string> int_string;
+
+const ll INF = LONG_LONG_MAX; // ll INF
+const int inf = INT_MAX; // int inf
+const int MOD = 1000000007;
+const int mod = 998244353;
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+    ll n;
+    cin >> n;
+//    int amax = pow(n,-3); //powは誤差が出る。
+    ll ans =0;
+    for(ll i = 1; i*i*i <= n;i++){
+        for(ll j = i; i*j*j <= n;j++){
+            ans+= (n/(i*j)) -j +1;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
