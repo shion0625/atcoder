58896ee0e84223c92cd806c20bf97880e65847d7
shion0625 <xkaito0912@gmail.com>
2022-04-10 12:10:30 +0000

    [AC] abc247 abc247_c

---
+++ atcoder.jp/abc247/abc247_c/Main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,b) for(int i=0; i<b; ++i)
+#define rep1(i,a,b) for(int i=a; i<b; ++i)
+#define rrep(i,a,b) for(int i=a; i>b; --i)
+
+#define repl(i,b) for(ll i=0; i<b; ++i)
+#define repl1(i,a,b) for(ll i=a; i<b; ++i)
+#define rrepl1(i,a,b) for(ll i=a; i>b; --i)
+
+# define ALL(x) (x).begin(), (x).end()
+# define SZ(x) ((int)(x).size)
+
+//エイリアス
+typedef long long ll;
+typedef long unsigned long ull;
+typedef long double ld;
+typedef pair<ll, ll> ll_pair;
+typedef pair<double, double> DoP;
+typedef pair<int, int> int_pair;
+typedef pair<int, string> int_string;
+typedef pair<string, int> string_int;
+
+
+const ll INF = LONG_LONG_MAX; // ll INF
+const int inf = INT_MAX; // int inf
+const int MOD = 1000000007;
+const int mod = 998244353;
+
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int n;
+    cin >> n;
+    string s= "1",ss="2";
+    rep1(i,1,n){
+        s = s+ " " + ss + " "+s;
+        ss = to_string(i+2);
+    }
+    cout << s << endl;
+    return 0;
+}
+
+
